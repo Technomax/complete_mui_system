4c88fcc2300c10c2139ca327901ba553193cda42
shadidabbasoptimum <shadi.dabbas@optimumpartners.co>
2022-03-19 12:12:25 -0500

    tooltip

--- src/components/MiniDrawer.js
+++ src/components/MiniDrawer.js
@@ -1,52 +1,61 @@
-import * as React from "react";
-import { Link } from "react-router-dom";
-import { styled, useTheme } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import MuiDrawer from "@mui/material/Drawer";
-import MuiAppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import List from "@mui/material/List";
-import CssBaseline from "@mui/material/CssBaseline";
-import Typography from "@mui/material/Typography";
-import Divider from "@mui/material/Divider";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
-import { Badge, ClickAwayListener, Collapse, formLabelClasses, Grid, InputBase, ListItem } from "@mui/material";
-import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
-import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
-import LogoutIcon from "@mui/icons-material/Logout";
-import SearchIcon from "@mui/icons-material/Search";
-import { makeStyles } from "@mui/styles";
-import IconProduct from "@mui/icons-material/ProductionQuantityLimits";
-import IconClient from "@mui/icons-material/CorporateFare";
-import IconExpandLess from "@mui/icons-material/ExpandLess";
-import IconExpandMore from "@mui/icons-material/ExpandMore";
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+import { styled, useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import MuiDrawer from '@mui/material/Drawer';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import CssBaseline from '@mui/material/CssBaseline';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import {
+  Badge,
+  ClickAwayListener,
+  Collapse,
+  formLabelClasses,
+  Grid,
+  InputBase,
+  ListItem,
+} from '@mui/material';
+import Tooltip, { tooltipClasses } from '@mui/material/Tooltip';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import LogoutIcon from '@mui/icons-material/Logout';
+import SearchIcon from '@mui/icons-material/Search';
+import { makeStyles } from '@mui/styles';
+import IconProduct from '@mui/icons-material/ProductionQuantityLimits';
+import IconClient from '@mui/icons-material/CorporateFare';
+import IconExpandLess from '@mui/icons-material/ExpandLess';
+import IconExpandMore from '@mui/icons-material/ExpandMore';
 
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    backgroundColor: "#fff",
-    transform: "translateZ(0)",
+    backgroundColor: '#fff',
+    transform: 'translateZ(0)',
   },
   title: {
-    color: "black",
+    color: 'black',
   },
   searchInput: {
-    opacity: "0.6",
+    opacity: '0.6',
     padding: `0px ${theme.spacing(1)}px`,
-    fontSize: "0.8rem",
-    "&:hover": {
-      backgroundColor: "#f2f2f2",
+    fontSize: '0.8rem',
+    '&:hover': {
+      backgroundColor: '#f2f2f2',
     },
-    "& .MuiSvgIcon-root": {
+    '& .MuiSvgIcon-root': {
       marginRight: theme.spacing(1),
     },
   },
@@ -54,46 +63,46 @@ const useStyles = makeStyles((theme) => ({
 
 const openedMixin = (theme) => ({
   width: drawerWidth,
-  transition: theme.transitions.create("width", {
+  transition: theme.transitions.create('width', {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.enteringScreen,
   }),
-  overflowX: "hidden",
+  overflowX: 'hidden',
 });
 
 const closedMixin = (theme) => ({
-  transition: theme.transitions.create("width", {
+  transition: theme.transitions.create('width', {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
-  overflowX: "hidden",
+  overflowX: 'hidden',
   width: `calc(${theme.spacing(7)} + 1px)`,
-  [theme.breakpoints.up("sm")]: {
+  [theme.breakpoints.up('sm')]: {
     width: `calc(${theme.spacing(8)} + 1px)`,
   },
 });
 
-const DrawerHeader = styled("div")(({ theme }) => ({
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "flex-end",
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-end',
   padding: theme.spacing(0, 1),
   // necessary for content to be below app bar
   ...theme.mixins.toolbar,
 }));
 
 const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== "open",
+  shouldForwardProp: (prop) => prop !== 'open',
 })(({ theme, open }) => ({
   zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(["width", "margin"], {
+  transition: theme.transitions.create(['width', 'margin'], {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
   ...(open && {
     marginLeft: drawerWidth,
     width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(["width", "margin"], {
+    transition: theme.transitions.create(['width', 'margin'], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.enteringScreen,
     }),
@@ -101,20 +110,29 @@ const AppBar = styled(MuiAppBar, {
 }));
 
 const Drawer = styled(MuiDrawer, {
-  shouldForwardProp: (prop) => prop !== "open",
+  shouldForwardProp: (prop) => prop !== 'open',
 })(({ theme, open }) => ({
   width: drawerWidth,
   flexShrink: 0,
-  whiteSpace: "nowrap",
-  boxSizing: "border-box",
+  whiteSpace: 'nowrap',
+  boxSizing: 'border-box',
   ...(open && {
     ...openedMixin(theme),
-    "& .MuiDrawer-paper": openedMixin(theme),
+    '& .MuiDrawer-paper': openedMixin(theme),
   }),
   ...(!open && {
     ...closedMixin(theme),
-    "& .MuiDrawer-paper": closedMixin(theme),
+    '& .MuiDrawer-paper': closedMixin(theme),
   }),
+}));
+
+const HtmlTooltip = styled(({ className, ...props }) => (
+  <Tooltip {...props} classes={{ popper: className }} />
+))(({ theme }) => ({
+  [`& .${tooltipClasses.tooltip}`]: {
+    backgroundColor: '#E2E3F3',
+    marginLeft:'0 !important',
+  },
 }));
 
 export default function MiniDrawer() {
@@ -135,7 +153,6 @@ export default function MiniDrawer() {
     setExpandB(false);
   };
 
-
   function handleClickA() {
     setExpandA(!expandA);
     setOpen(true);
@@ -148,134 +165,151 @@ export default function MiniDrawer() {
 
   return (
     <ClickAwayListener onClickAway={handleDrawerClose}>
-    <Box sx={{ display: "flex" }}>
-      <AppBar position="fixed" open={open} className={classes.root}>
-        <Toolbar>
-          <IconButton
-            color="info"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            sx={{
-              marginRight: 5,
-              ...(open && { display: "none" }),
-            }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Grid container alignItems="center">
-            <Grid item>
-              <Typography
-                variant="h6"
-                noWrap
-                component="div"
-                className={classes.title}
-              >
-               Web Store
-              </Typography>
-            </Grid>
-            <Grid item sm></Grid>
-            <Grid item>
-              <InputBase
-                placeholder="Search topics"
-                className={classes.searchInput}
-                startAdornment={<SearchIcon fontSize="small" />}
-              />
-              <IconButton>
-                <Badge badgeContent={4} color="secondary">
-                  <NotificationsNoneIcon fontSize="small" />
-                </Badge>
-              </IconButton>
-              <IconButton>
-                <Badge badgeContent={3} color="primary">
-                  <ChatBubbleOutlineIcon fontSize="small" />
-                </Badge>
-              </IconButton>
-              <IconButton>
-                <LogoutIcon fontSize="small" />
-              </IconButton>
-            </Grid>
-          </Grid>
-        </Toolbar>
-      </AppBar>
-      <Drawer variant="permanent" open={open}>
-        <DrawerHeader>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === "rtl" ? (
-              <ChevronRightIcon />
-            ) : (
-              <ChevronLeftIcon />
-            )}
-          </IconButton>
-        </DrawerHeader>
-        <Divider />
-        <List>
-          {["Dashboard", "Home", "About Us", "Contact"].map((text, index) => (
-            <ListItemButton
-              key={text}
+      <Box sx={{ display: 'flex' }}>
+        <AppBar position="fixed" open={open} className={classes.root}>
+          <Toolbar>
+            <IconButton
+              color="info"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              edge="start"
               sx={{
-                minHeight: 48,
-                justifyContent: open ? "initial" : "center",
-                px: 2.5,
-              }}
-            >
-              <ListItemIcon
+                marginRight: 5,
+                ...(open && { display: 'none' }),
+              }}>
+              <MenuIcon />
+            </IconButton>
+            <Grid container alignItems="center">
+              <Grid item>
+                <Typography
+                  variant="h6"
+                  noWrap
+                  component="div"
+                  className={classes.title}>
+                  Web Store
+                </Typography>
+              </Grid>
+              <Grid item sm></Grid>
+              <Grid item>
+                <InputBase
+                  placeholder="Search topics"
+                  className={classes.searchInput}
+                  startAdornment={<SearchIcon fontSize="small" />}
+                />
+                <IconButton>
+                  <Badge badgeContent={4} color="secondary">
+                    <NotificationsNoneIcon fontSize="small" />
+                  </Badge>
+                </IconButton>
+                <IconButton>
+                  <Badge badgeContent={3} color="primary">
+                    <ChatBubbleOutlineIcon fontSize="small" />
+                  </Badge>
+                </IconButton>
+                <IconButton>
+                  <LogoutIcon fontSize="small" />
+                </IconButton>
+              </Grid>
+            </Grid>
+          </Toolbar>
+        </AppBar>
+        <Drawer variant="permanent" open={open}>
+          <DrawerHeader>
+            <IconButton onClick={handleDrawerClose}>
+              {theme.direction === 'rtl' ? (
+                <ChevronRightIcon />
+              ) : (
+                <ChevronLeftIcon />
+              )}
+            </IconButton>
+          </DrawerHeader>
+          <Divider />
+          <List>
+            {['Dashboard', 'Home', 'About Us', 'Contact'].map((text, index) => (
+              <ListItemButton
+                key={text}
                 sx={{
-                  minWidth: 0,
-                  mr: open ? 3 : "auto",
-                  justifyContent: "center",
-                }}
-              >
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  minHeight: 48,
+                  justifyContent: open ? 'initial' : 'center',
+                  px: 2.5,
+                }}>
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: open ? 3 : 'auto',
+                    justifyContent: 'center',
+                  }}>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
+              </ListItemButton>
+            ))}
+          </List>
+          <Divider />
+
+          <HtmlTooltip
+            placement="right-start"
+            title={
+                <List component="div" disablePadding>
+                  <ListItem button className={classes.menuItem}>
+                    <Link className="option" to="/eccutils">
+                      <ListItemText inset primary="EccUtils" />
+                    </Link>
+                  </ListItem>
+                  <ListItem button className={classes.menuItem}>
+                    <Link className="option" to="/ciclreporting">
+                      <ListItemText inset primary="CICL Reporting" />
+                    </Link>
+                  </ListItem>
+                </List>
+            }>
+            <ListItem
+              button
+              onClick={handleClickA}
+              className={classes.menuItem}>
+              <ListItemIcon className={classes.menuItemIcon}>
+                <IconProduct />
               </ListItemIcon>
-              <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
-            </ListItemButton>
-          ))}
-        </List>
-        <Divider />
-        <ListItem button onClick={handleClickA} className={classes.menuItem}>
-          <ListItemIcon className={classes.menuItemIcon}>
-            <IconProduct />
-          </ListItemIcon>
-          <ListItemText primary="Products" />
-          {expandA ? <IconExpandLess /> : <IconExpandMore />}
-        </ListItem>
-        <Collapse in={expandA} timeout="auto" unmountOnExit>
+              <ListItemText primary="Products" />
+              {expandA ? <IconExpandLess /> : <IconExpandMore />}
+            </ListItem>
+          </HtmlTooltip>
+          <Collapse in={expandA} timeout="auto" unmountOnExit>
+            <Divider />
+            <List component="div" disablePadding>
+              <ListItem button className={classes.menuItem}>
+                <Link className="option" to="/eccutils">
+                  <ListItemText inset primary="EccUtils" />
+                </Link>
+              </ListItem>
+              <ListItem button className={classes.menuItem}>
+                <Link className="option" to="/ciclreporting">
+                  <ListItemText inset primary="CICL Reporting" />
+                </Link>
+              </ListItem>
+            </List>
+          </Collapse>
           <Divider />
-          <List component="div" disablePadding>
-            <ListItem button className={classes.menuItem}>
-              <Link className="option" to="/eccutils">
-                <ListItemText inset primary="EccUtils" />
-              </Link>
-            </ListItem>
-            <ListItem button className={classes.menuItem}>
-              <Link className="option" to="/ciclreporting">
-                <ListItemText inset primary="CICL Reporting" />
-              </Link>
-            </ListItem>
-          </List>
-        </Collapse>
-        <Divider />
-        <ListItem button onClick={handleClickB} className={classes.menuItem}>
-          <ListItemIcon className={classes.menuItemIcon}>
-            <IconClient />
-          </ListItemIcon>
-          <ListItemText primary="Clients" />
-          {expandB ? <IconExpandLess /> : <IconExpandMore />}
-        </ListItem>
-        <Collapse in={expandB} timeout="auto" unmountOnExit>
-          <Divider />
-          <List component="div" disablePadding>
-            <ListItem button className={classes.menuItem}>
-              <ListItemText inset primary="Nabil Bank" />
-            </ListItem>
-            <ListItem button className={classes.menuItem}>
-              <ListItemText inset primary="NMB Bank" />
-            </ListItem>
-          </List>
-        </Collapse>
-      </Drawer>
-    </Box>
+          <ListItem button onClick={handleClickB} className={classes.menuItem}>
+            <ListItemIcon className={classes.menuItemIcon}>
+              <IconClient />
+            </ListItemIcon>
+            <ListItemText primary="Clients" />
+            {expandB ? <IconExpandLess /> : <IconExpandMore />}
+          </ListItem>
+          <Collapse in={expandB} timeout="auto" unmountOnExit>
+            <Divider />
+            <List component="div" disablePadding>
+              <ListItem button className={classes.menuItem}>
+                <ListItemText inset primary="Nabil Bank" />
+              </ListItem>
+              <ListItem button className={classes.menuItem}>
+                <ListItemText inset primary="NMB Bank" />
+              </ListItem>
+            </List>
+          </Collapse>
+        </Drawer>
+      </Box>
     </ClickAwayListener>
   );
 }
